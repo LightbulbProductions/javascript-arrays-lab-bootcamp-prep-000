c33e829fb315df28da41e858ce24b5746cc1282c
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 12:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,14 @@ function destructivelyPrependKitten(name){
    return [...kittens, name];
  }
  
- function prependKitten(name) 
- 
+ function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, 3);
+}
